6ed5f734f9ff7af47df1948eca4e45e4f79bdf37
hariyadav-08 <hariyaadav143@gmail.com>
2020-08-05 13:10:13 +0530

    addedmyfiles

---
+++ src/main/java/com/code/devopsILP1.java
@@ -0,0 +1,15 @@
+package com.code;
+
+public class devopsILP1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Welcome to Devops Training");
+		System.out.println(" This is Devops profession certification");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
